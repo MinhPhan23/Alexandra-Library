541a9dd72fa7d7be9f89595e26893434b09ecf7d
xiang <shix2@myumanitoba.ca>
2024-02-03 02:59:53 -0600

    search bar input layer Done

---
+++ app/src/main/java/com/alexandria_library/presentation/SearchBarInput.java
@@ -0,0 +1,29 @@
+package com.alexandria_library.presentation;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class SearchBarInput {
+    public static void setupSearchBar(EditText searchBarInput, final SearchBarListener listener){
+        searchBarInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                listener.onTextChanged(s.toString());
+            }
+        });
+    }
+    public interface SearchBarListener {
+        void onTextChanged(String input);
+    }
+}
