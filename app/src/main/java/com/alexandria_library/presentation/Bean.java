2dd7f1fac3d1d4cf6d6dc8dd0ebeb9a9168d239b
Xiang Shi <shix2@myumanitoba.ca>
2024-02-03 16:23:51 -0600

    Vertical List of book display UI done

---
+++ app/src/main/java/com/alexandria_library/presentation/Bean.java
@@ -0,0 +1,14 @@
+package com.alexandria_library.presentation;
+
+public class Bean {
+
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- app/src/main/java/com/alexandria_library/presentation/MainActivity.java
+++ app/src/main/java/com/alexandria_library/presentation/MainActivity.java
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.alexandria_library.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements SearchBarInput.SearchBarListener {
 
+    private List<Bean> data = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +26,31 @@ public class MainActivity extends AppCompatActivity implements SearchBarInput.Se
         //Getting Search Bar input immediately
         EditText editText = findViewById(R.id.searchInput);
         SearchBarInput.setupSearchBar(editText, this);
+
+        //Setting Horizontal List of book display
+        getBookData(data);
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(new MyAdapter(data, this));
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.e("xiang", "onClicked: "+position);
+            }
+        });
+
     }
 
     @Override
     public void onTextChanged(String input){
         Log.e("xiang", "New Input: "+input);
+    }
+
+    public void getBookData (List<Bean> data){
+        for(int i = 0; i<100; i++){
+            Bean bean = new Bean();
+            bean.setName("Book"+i);
+            data.add(bean);
+        }
     }
 }
