f0893878b4e7053e6c29971fc1ebb199ab22791a
Minh Phan <minhsg.phan@gmail.com>
2024-02-04 21:50:22 -0600

    Create a body for implementation

--- app/src/main/java/com/alexandria_library/dso/Book.java
+++ app/src/main/java/com/alexandria_library/dso/Book.java
@@ -1,4 +1,63 @@
 package com.alexandria_library.dso;
 
-public class Book {
+public class Book implements IBook{
+    @Override
+    public void setID(int id) {
+
+    }
+
+    @Override
+    public int getID() {
+        return 0;
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setAuthor(String author) {
+
+    }
+
+    @Override
+    public String getAuthor() {
+        return null;
+    }
+
+    @Override
+    public void setTags(String[] tags) {
+
+    }
+
+    @Override
+    public String[] getTags() {
+        return new String[0];
+    }
+
+    @Override
+    public void setGenres(String[] genres) {
+
+    }
+
+    @Override
+    public String[] getGenres() {
+        return new String[0];
+    }
+
+    @Override
+    public void setDate(String date) {
+
+    }
+
+    @Override
+    public String getDate() {
+        return null;
+    }
 }
