0a956eaa5baec6b970839b49dfe3f02103b26577
Minh Phan <minhsg.phan@gmail.com>
2024-02-04 21:48:55 -0600

    Create unit test for search book by name

---
+++ app/src/test/java/com/alexandria_library/logic/SearchServiceTest.java
@@ -0,0 +1,35 @@
+package com.alexandria_library.logic;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import com.alexandria_library.logic.SearchService;
+import com.alexandria_library.dso.*;
+
+public class SearchServiceTest {
+    private SearchService searchService;
+    @Before
+    public void setUp() {
+        System.out.println("Starting unit tests for SearchService");
+        searchService = new SearchService();
+        assertNotNull(searchService);
+    }
+
+    @Test
+    public void testSearchByName() {
+        System.out.println("Testing searching for books by name");
+        String keywords = "The Three Musketeers";
+        String[] keyword = keywords.split(" ");
+        Book[] bookList = searchService.searchInput(keywords);
+        for (Book book : bookList) {
+            String bookName = book.getName();
+            boolean check = false;
+            for (String word : keyword) {
+                if (bookName.contains(word)) {
+                    check = true;
+                    break;
+                }
+            }
+            assertTrue(check);
+        }
+    }
+}
