65b1963e1313072a868a93a280be18e5a45fea5a
Minh Phan <minhsg.phan@gmail.com>
2024-02-05 23:58:07 -0600

    test for exception throws

--- app/src/test/java/com/alexandria_library/logic/SearchServiceTest.java
+++ app/src/test/java/com/alexandria_library/logic/SearchServiceTest.java
@@ -1,6 +1,7 @@
 package com.alexandria_library.logic;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 import com.alexandria_library.dso.Book;
@@ -14,27 +15,58 @@ public class SearchServiceTest {
     private SearchService searchService;
     @Before
     public void setUp() {
-        System.out.println("Starting unit tests for SearchService");
+        System.out.println("Starting tests for SearchService");
         searchService = new SearchService();
         assertNotNull(searchService);
     }
 
     @Test
+    public void testEmptySearch() {
+        System.out.println("Test empty keywords String");
+        Exception exception = assertThrows(SearchServiceException.class, () -> {
+            searchService.searchInput("");
+        });
+
+        String expectedMessage = "Could not search for empty text";
+        String actualMessage = exception.getMessage();
+
+        assertNotNull(actualMessage);
+        assertTrue(actualMessage.contains(expectedMessage));
+
+        System.out.println("Test null String object");
+        exception = assertThrows(SearchServiceException.class, () -> {
+            searchService.searchInput(null);
+        });
+        expectedMessage = "Could not search for empty text";
+        actualMessage = exception.getMessage();
+
+        assertNotNull(actualMessage);
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    /**
+     * This is an integration test
+     */
+    @Test
     public void testSearchByName() {
         System.out.println("Testing searching for books by name");
         String keywords = "The Three Alexander";
         String[] keyword = keywords.split(" ");
-        ArrayList<Book> bookList = searchService.searchInput(keywords);
-        for (Book book : bookList) {
-            String bookName = book.getName();
-            boolean check = false;
-            for (String word : keyword) {
-                if (bookName.contains(word)) {
-                    check = true;
-                    break;
+        try {
+            ArrayList<Book> bookList = searchService.searchInput(keywords);
+            for (Book book : bookList) {
+                String bookName = book.getName();
+                boolean check = false;
+                for (String word : keyword) {
+                    if (bookName.contains(word)) {
+                        check = true;
+                        break;
+                    }
                 }
+                assertTrue(check);
             }
-            assertTrue(check);
+        } catch (SearchServiceException ignored) {
+
         }
     }
 }
