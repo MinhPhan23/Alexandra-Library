732b6ae9369cd98fa3fa73d7b85ed2a2a55096eb
xiang <shix2@myumanitoba.ca>
2024-02-05 19:16:23 -0600

    created User dso,logic,data, and service Note: also fixed BookPersistent & BookPersistentIntermediate 's rename bug

--- app/src/main/java/com/alexandria_library/dso/User.java
+++ app/src/main/java/com/alexandria_library/dso/User.java
@@ -1,4 +1,37 @@
 package com.alexandria_library.dso;
 
-public class User {
+import java.util.ArrayList;
+
+public class User{
+    private ArrayList<Book> allBookList;
+    private ArrayList<Book> inProgressList;
+    private ArrayList<Book> finishedList;
+    private String userName;
+    private String password;
+
+
+    public User(String userName, String password){
+        this.userName = userName;
+        this.password = password;
+        allBookList = new ArrayList<>();
+        inProgressList = new ArrayList<>();
+        finishedList = new ArrayList<>();
+    }
+
+    public ArrayList<Book> getAllBookList(){
+        return allBookList;
+    }
+    public ArrayList<Book> getInProgressList(){
+        return inProgressList;
+    }
+    public ArrayList<Book> getFinishedList(){
+        return finishedList;
+    }
+    public String getUserName(){
+        return userName;
+    }
+    public String getPassword(){
+        return password;
+    }
+
 }
