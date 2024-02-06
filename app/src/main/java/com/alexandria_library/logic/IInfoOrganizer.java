710327970348ac649624eb52181d25afaa86eb63
xiang <shix2@myumanitoba.ca>
2024-02-05 20:57:42 -0600

    created register and login page UI

--- app/src/main/java/com/alexandria_library/logic/IInfoOrganizer.java
+++ app/src/main/java/com/alexandria_library/logic/IInfoOrganizer.java
@@ -1,23 +1,23 @@
 package com.alexandria_library.logic;
 
-import com.alexandria_library.dso.BookList;
+//import com.alexandria_library.dso.BookList;
 
 public interface IInfoOrganizer {
     //sort books alphabetically
-    public BookList sortByTitle(BookList bookList);
-
-    //sort books by date published
-    public BookList sortByDate(BookList bookList);
-
-    //sort books by author alphabetically
-    public BookList sortByAuthor(BookList bookList);
-
-    //filter books by tag
-    public BookList filterByTag(BookList bookList, String[] tags);
-
-    //filter books by genre
-    public BookList filterByGenre(BookList bookList, String[] genres);
-
-    //filter books by author
-    public BookList filterByAuthor(BookList bookList, String[] authors);
+//    public BookList sortByTitle(BookList bookList);
+//
+//    //sort books by date published
+//    public BookList sortByDate(BookList bookList);
+//
+//    //sort books by author alphabetically
+//    public BookList sortByAuthor(BookList bookList);
+//
+//    //filter books by tag
+//    public BookList filterByTag(BookList bookList, String[] tags);
+//
+//    //filter books by genre
+//    public BookList filterByGenre(BookList bookList, String[] genres);
+//
+//    //filter books by author
+//    public BookList filterByAuthor(BookList bookList, String[] authors);
 }
