e00c8f5c964541f0d5bb3d96c3ad38c78e290995
Minh Phan <minhsg.phan@gmail.com>
2024-02-05 22:02:51 -0600

    Implement search logic

--- app/src/main/java/com/alexandria_library/logic/SearchService.java
+++ app/src/main/java/com/alexandria_library/logic/SearchService.java
@@ -1,18 +1,34 @@
 package com.alexandria_library.logic;
 
+import com.alexandria_library.data.IBookPersistent;
+import com.alexandria_library.data.IBookPersistentIntermediate;
 import com.alexandria_library.dso.Book;
 
 import java.util.ArrayList;
 
 public class SearchService implements ISearchService{
+    BookPersistentIntermediate data;
     public SearchService() {
-
+        data = new BookPersistentIntermediate();
     }
 
     @Override
     public ArrayList<Book> searchInput(String keywords) {
-        ArrayList<Book> result = null;
+        ArrayList<Book> result = new ArrayList<Book>();
         String[] keywordList = keywords.split(" ");
+        ArrayList<Book> bookByName = data.searchByName(keywordList);
+        ArrayList<Book> bookByAuthor = data.searchByAuthor(keywordList);
+        ArrayList<Book> bookByGenre = data.searchByGenre(keywordList);
+        ArrayList<Book> bookByTag = data.searchByTag(keywordList);
+        try {
+            result.addAll(bookByName);
+            result.addAll(bookByAuthor);
+            result.addAll(bookByGenre);
+            result.addAll(bookByTag);
+        }
+        catch (NullPointerException e) {
+            System.out.println(e);
+        }
         return result;
     }
 
