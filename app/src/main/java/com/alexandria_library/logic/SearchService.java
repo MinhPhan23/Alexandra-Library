e2c55fbe88c72015fa54a596e27b63b2e7ee4d8d
Minh Phan <minhsg.phan@gmail.com>
2024-02-05 23:58:38 -0600

    implement searching books

--- app/src/main/java/com/alexandria_library/logic/SearchService.java
+++ app/src/main/java/com/alexandria_library/logic/SearchService.java
@@ -1,21 +1,30 @@
 package com.alexandria_library.logic;
 
-import com.alexandria_library.data.IBookPersistent;
-import com.alexandria_library.data.IBookPersistentIntermediate;
 import com.alexandria_library.dso.Book;
 
 import java.util.ArrayList;
 
 public class SearchService implements ISearchService{
-    BookPersistentIntermediate data;
+    private final BookPersistentIntermediate data;
+    private final InfoOrganizer infoOrganizer;
     public SearchService() {
         data = new BookPersistentIntermediate();
+        infoOrganizer = new InfoOrganizer();
     }
 
     @Override
-    public ArrayList<Book> searchInput(String keywords) {
-        ArrayList<Book> result = new ArrayList<Book>();
+    public ArrayList<Book> searchInput(String keywords) throws SearchServiceException {
+        if (keywords == null || keywords.length() == 0) {
+            throw new SearchServiceException("Could not search for empty text");
+        }
         String[] keywordList = keywords.split(" ");
+        ArrayList<Book> result = queryDatabase(keywordList);
+        result = infoOrganizer.sort(result, keywordList);
+        return result;
+    }
+
+    private ArrayList<Book> queryDatabase(String[] keywordList) throws SearchServiceException{
+        ArrayList<Book> result = new ArrayList<>();
         ArrayList<Book> bookByName = data.searchByName(keywordList);
         ArrayList<Book> bookByAuthor = data.searchByAuthor(keywordList);
         ArrayList<Book> bookByGenre = data.searchByGenre(keywordList);
@@ -27,15 +36,9 @@ public class SearchService implements ISearchService{
             result.addAll(bookByTag);
         }
         catch (NullPointerException e) {
-            System.out.println(e);
+            e.printStackTrace();
+            throw new SearchServiceException("Could not search for the result, please try again");
         }
         return result;
     }
-
-    @Override
-    public ArrayList<Book> filterInput(String[] filters) {
-        ArrayList<Book> result = null;
-        return result;
-    }
-
 }
