5831657d1e28f21a21d1a9e28d3dfcbfe9bb9439
Minh Phan <minhsg.phan@gmail.com>
2024-02-08 00:12:06 -0600

    Update all unit test

--- app/src/test/java/com/alexandria_library/tests/AllUnitTests.java
+++ app/src/test/java/com/alexandria_library/tests/AllUnitTests.java
@@ -8,11 +8,15 @@ import org.junit.runners.Suite;
 
 
 //import all test files
+import com.alexandria_library.tests.logic.AuthenticationTest;
+import com.alexandria_library.tests.logic.InfoOrganizerTest;
 import com.alexandria_library.tests.logic.SearchServiceTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        SearchServiceTest.class
+        SearchServiceTest.class,
+        InfoOrganizerTest.class,
+        AuthenticationTest.class
 })
 
 public class AllUnitTests {
