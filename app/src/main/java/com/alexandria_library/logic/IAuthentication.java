18cdda4111abfb389f971639051c5777ed66e422
Minh Phan <minhsg.phan@gmail.com>
2024-03-02 18:31:28 -0600

    Add Authentication Interface
    Resolved: #52

--- app/src/main/java/com/alexandria_library/logic/IAuthentication.java
+++ app/src/main/java/com/alexandria_library/logic/IAuthentication.java
@@ -3,6 +3,6 @@ package com.alexandria_library.logic;
 import com.alexandria_library.dso.User;
 
 public interface IAuthentication {
-    public User findExist(String userName, String password);
-    public boolean insertNewUser(String userName, String password);
+    public User login(String userName, String password) throws AuthenticationException;
+    public void register(String userName, String password, String doublePassword) throws AuthenticationException;
 }
