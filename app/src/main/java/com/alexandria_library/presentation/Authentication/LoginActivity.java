7e2ac848e29d53e16dd56e75c23316aa2b48369c
xiang <shix2@myumanitoba.ca>
2024-03-05 03:36:28 -0600

    build one class HSQLDBHelper.java to store copyDatabaseToDevice function in program start. that's will easier to add more feature to DBHelper on the future.

--- app/src/main/java/com/alexandria_library/presentation/Authentication/LoginActivity.java
+++ app/src/main/java/com/alexandria_library/presentation/Authentication/LoginActivity.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 
 import com.alexandria_library.R;
 import com.alexandria_library.application.Main;
+import com.alexandria_library.data.utils.HSQLDBHelper;
 import com.alexandria_library.dso.User;
 import com.alexandria_library.logic.Authentication;
 import com.alexandria_library.logic.AuthenticationException;
@@ -39,7 +40,8 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         find();
         authentication = new Authentication();
-        copyDatabaseToDevice();
+
+        HSQLDBHelper.copyDatabaseToDevice(this); //copy database
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -105,58 +107,5 @@ public class LoginActivity extends AppCompatActivity {
     }
     private void setErrorMess(EditText layout, String message){
         layout.setError(message);
-    }
-
-
-    /******
-     * Copy database to device at login page, cause login page is first page landed
-     */
-    private void copyDatabaseToDevice(){
-        final String DB_Path = "db";
-        String [] assetNames;
-        Context context = getApplicationContext();
-        File dataDirectory = context.getDir(DB_Path, Context.MODE_PRIVATE);
-        AssetManager assetManager = getAssets();
-
-        try{
-            assetNames = assetManager.list(DB_Path);
-            for(int i = 0; i<assetNames.length; i++){
-                assetNames[i] = DB_Path + "/" + assetNames[i];
-            }
-            copyAssetsToDirectory(assetNames, dataDirectory);
-            Main.setDBPathName(dataDirectory.toString() + "/" + Main.getDBPathName());
-
-        } catch(final IOException e){
-            System.out.println("error in copy database to device");
-            e.printStackTrace();
-        }
-    }
-
-    public void copyAssetsToDirectory(String [] assets, File directory) throws IOException{
-        AssetManager assetManager = getAssets();
-
-        for (String asset : assets) {
-            String[] components = asset.split("/");
-            String copyPath = directory.toString() + "/" + components[components.length - 1];
-
-            char[] buffer = new char[1024];
-            int count;
-
-            File outFile = new File(copyPath);
-
-            if (!outFile.exists()) {
-                InputStreamReader in = new InputStreamReader(assetManager.open(asset));
-                FileWriter out = new FileWriter(outFile);
-
-                count = in.read(buffer);
-                while (count != -1) {
-                    out.write(buffer, 0, count);
-                    count = in.read(buffer);
-                }
-
-                out.close();
-                in.close();
-            }
-        }
     }
 }
