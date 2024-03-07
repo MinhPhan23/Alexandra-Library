fad0dd91d795e6e3f58e0c4d1d03ee17ac64c671
xiang <shix2@myumanitoba.ca>
2024-03-06 18:22:18 -0600

    built for FilterBookAdapter.java

--- app/src/main/java/com/alexandria_library/presentation/Adapter/FilterBookAdapter.java
+++ app/src/main/java/com/alexandria_library/presentation/Adapter/FilterBookAdapter.java
@@ -1,14 +1,91 @@
 package com.alexandria_library.presentation.Adapter;
 
+import android.content.Context;
+import android.view.GestureDetector;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ConcatAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.alexandria_library.R;
+import com.alexandria_library.dso.Booklist;
+import com.alexandria_library.logic.BookListFilter;
+import com.alexandria_library.logic.IBookListFilter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FilterBookAdapter extends RecyclerView.Adapter<FilterBookAdapter.MyViewHolder> {
+    private static IBookListFilter filter;
+    private Booklist filteredBooks;
+    private Booklist allLibraryBooks;
+    private String [] tagArray;
+    private String [] genreArray;
+    private Context context;
 
+    public FilterBookAdapter(Context context, IBookListFilter filter, Booklist allLibraryBooks, ArrayList<String>tagsList,
+                             ArrayList<String> genresList){
+
+        tagArray = transferListToArray(tagsList);
+        genreArray = transferListToArray(genresList);
+        this.allLibraryBooks = allLibraryBooks;
+        initializer(context, filter);
+        findBooks();
+    }
+    public void initializer(Context context, IBookListFilter filter){
+        FilterBookAdapter.filter = filter;
+        this.context = context;
+    }
+
+    public void findBooks(){
+        if(filter != null){
+            filteredBooks = filter.getFilteredList(allLibraryBooks, tagArray, genreArray);
+        }
+        else{
+            filteredBooks = null;
+        }
+    }
+
+    public String[] transferListToArray(ArrayList<String> list){
+        String[] result = new String[list.size()];
+        if(result.length > 0){
+            result = list.toArray(result);
+        }
+        return result;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        View view = View.inflate(context, R.layout.activity_filter_book_item, null);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FilterBookAdapter.MyViewHolder holder, int position){
+        //get book name
+        holder.title.setText(filteredBooks.get(position).getName());
+
+        //get book id
+        String parsedID = ""+filteredBooks.get(position).getID();
+        holder.id.setText(parsedID);
+
+        //get tags
+        String tags = filteredBooks.get(position).getTags().toString();
+        holder.tag.setText(tags);
+
+        //get genres
+        String genres = filteredBooks.get(position).getGenres().toString();
+        holder.genre.setText(genres);
+    }
+
+    @Override
+    public int getItemCount(){
+        return filteredBooks.size();
+    }
 
     private OnRecyclerItemClickListener myOnItemClickListener;
     public class MyViewHolder extends RecyclerView.ViewHolder {
@@ -19,8 +96,10 @@ public class FilterBookAdapter extends RecyclerView.Adapter<FilterBookAdapter.My
 
         public MyViewHolder(View itemView){
             super(itemView);
-            title = itemView.findViewById(R.id.book_title);
-            id = itemView.findViewById(R.id.book_id);
+            title = itemView.findViewById(R.id.filter_book_title);
+            id = itemView.findViewById(R.id.filter_book_id);
+            tag = itemView.findViewById(R.id.filter_book_tags);
+            genre = itemView.findViewById(R.id.filter_book_genres);
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
