da05d243730617e0ec86a08843ced170d099a188
Carlo Garcia <garciac4@myumanitoba.ca>
2024-03-07 12:20:34 -0600

    fix IT test suite

--- app/src/test/java/com/alexandria_library/tests/IntegrationTests.java
+++ app/src/test/java/com/alexandria_library/tests/IntegrationTests.java
@@ -1,12 +1,14 @@
 package com.alexandria_library.tests;
-import com.alexandria_library.tests.data.BookListFilterIT;
+import com.alexandria_library.tests.logic.BookListFilterITtest;
+import com.alexandria_library.tests.logic.SearchServiceIT;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        BookListFilterIT.class
+        BookListFilterITtest.class,
+        SearchServiceIT.class
 })
 public class IntegrationTests {
 }
