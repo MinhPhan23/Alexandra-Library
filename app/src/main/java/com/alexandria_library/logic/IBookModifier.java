8c3fe22415ca5cd790dd0c96c18dd6c2b53a16e1
Xiang <shix2@myumanitoba.ca>
2024-03-21 00:40:25 -0500

    Created BookModifier.java in logic, used to contain add, delete, update, new book. Implemented processImage function to work transifer image's uri to byte and send to database.

---
+++ app/src/main/java/com/alexandria_library/logic/IBookModifier.java
@@ -0,0 +1,9 @@
+package com.alexandria_library.logic;
+
+import android.content.Context;
+import android.net.Uri;
+
+public interface IBookModifier {
+
+    public void processImage(Context context, Uri imageUri);
+}
