07d077304bec4db3bca7e7c01ab0c6e731d2c95e
Minh Phan <minhsg.phan@gmail.com>
2024-03-21 23:32:32 -0500

    Add DefaultBooklist.java unit tests
    Resolved: #134
    See also: #71

--- app/src/test/java/com/alexandria_library/tests/logic/DefaultBooklistTest.java
+++ app/src/test/java/com/alexandria_library/tests/logic/DefaultBooklistTest.java
@@ -1,60 +1,372 @@
 package com.alexandria_library.tests.logic;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
 
+import com.alexandria_library.data.IUserPersistent;
 import com.alexandria_library.dso.Book;
+import com.alexandria_library.dso.Booklist;
 import com.alexandria_library.dso.Reader;
-import com.alexandria_library.logic.CustomBooklist;
+import com.alexandria_library.dso.User;
 import com.alexandria_library.logic.DefaultBooklist;
+import com.alexandria_library.logic.Exception.BooklistException;
 
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 public class DefaultBooklistTest {
+    @Mock
     private Reader reader;
-    private DefaultBooklist defaultBooklist;
+    @Mock
+    private IUserPersistent data;
+    @Mock
     private Book book1, book2;
+    private Booklist oldList, newList;
+    private DefaultBooklist defaultBooklist;
     @Before
     public void setUp() {
-        defaultBooklist = new DefaultBooklist();
+        MockitoAnnotations.openMocks(this);
 
-        String[] tagsArray = new String[]{"Young Adult", "Twilight 1", "Vampires"};
-        String[] genreArray = new String[]{"Urban Fantasy", "Paranormal", "Romance"};
-        String[] tagsArray2 =  new String[]{"Post Apocalyptic", "Survival", "Hunger Games 1"};
-        String[] genreArray2 = new String[]{"Dystopia", "Science Fiction", "Fantasy", "Young Adult", "Action"};
-        List<String> tags = new ArrayList<>(Arrays.asList(tagsArray));
-        List<String> genres = new ArrayList<>(Arrays.asList(genreArray));
-        List<String> tags2 = new ArrayList<>(Arrays.asList(tagsArray2));
-        List<String> genres2 = new ArrayList<>(Arrays.asList(genreArray2));
+        Mockito.doNothing().when(data).addBookToCustomList(Mockito.any(Booklist.class), Mockito.any(User.class));
+        Mockito.doNothing().when(data).addBookToFinishedList(Mockito.any(Booklist.class), Mockito.any(User.class));
+        Mockito.doNothing().when(data).addBookToReadingList(Mockito.any(Booklist.class), Mockito.any(User.class));
 
-        book1 = new Book(1 ,
-                "Twilight",
-                "Stephenie Meyer",
-                "October 5, 2005",
-                tags,
-                genres);
+        defaultBooklist = new DefaultBooklist(data);
 
-        book2 = new Book(5 ,
-                "The Hunger Games",
-                "Suzanne Collins",
-                "September 14, 2008",
-                tags2,
-                genres2);
+        newList = new Booklist();
+        oldList = new Booklist();
 
         assertNotNull(defaultBooklist);
     }
 
     @Test
     public void testAddBookToAll() {
+        try {
+            Mockito.when(book1.getName()).thenReturn("Twilight");
+            Mockito.when(book2.getName()).thenReturn("Jedi");
 
+            newList.add(book1);
+            oldList.add(book2);
+
+            Mockito.when(reader.getAllBooksList()).thenReturn(oldList);
+
+            defaultBooklist.addBookToAll(reader, newList);
+
+            Mockito.verify(data, Mockito.times(1)).addBookToCustomList(Mockito.any(Booklist.class), Mockito.any(User.class));
+            Mockito.verify(reader).getAllBooksList();
+            assertEquals(2, oldList.size());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            assert(false);
+        }
+    }
+
+    @Test
+    public void testAddBooklistToAll() {
+        try {
+            Mockito.when(book1.getName()).thenReturn("Twilight");
+            Mockito.when(book2.getName()).thenReturn("Jedi");
+
+            newList.add(book1);
+            newList.add(book2);
+
+            Mockito.when(reader.getAllBooksList()).thenReturn(oldList);
+
+            defaultBooklist.addBookToAll(reader, newList);
+
+            Mockito.verify(data, Mockito.times(1)).addBookToCustomList(Mockito.any(Booklist.class), Mockito.any(User.class));
+            Mockito.verify(reader).getAllBooksList();
+            assertEquals(2, oldList.size());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            assert(false);
+        }
     }
 
     @Test
     public void testAddDuplicateBookToAll() {
+        try {
+            Mockito.when(book1.getName()).thenReturn("Twilight");
 
+            oldList.setName("all");
+            newList.add(book1);
+            oldList.add(book1);
+
+            Mockito.when(reader.getAllBooksList()).thenReturn(oldList);
+
+            Exception exception = assertThrows(BooklistException.class, () -> {
+                defaultBooklist.addBookToAll(reader, newList);
+            });
+
+            String expectedMessage = "The book(s) Twilight is already in list all";
+            String actualMessage = exception.getMessage();
+
+            assertNotNull(actualMessage);
+            assertEquals(expectedMessage, actualMessage);
+
+            Mockito.verify(data, Mockito.times(0)).addBookToCustomList(Mockito.any(Booklist.class), Mockito.any(User.class));
+            Mockito.verify(reader).getAllBooksList();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            assert(false);
+        }
+    }
+
+    @Test
+    public void testAddMultipleDuplicateBookToAll() {
+        try {
+            Mockito.when(book1.getName()).thenReturn("Twilight");
+            Mockito.when(book2.getName()).thenReturn("Jedi");
+
+            oldList.setName("all");
+            newList.add(book1);
+            newList.add(book2);
+            oldList.add(book1);
+            oldList.add(book2);
+
+            Mockito.when(reader.getAllBooksList()).thenReturn(oldList);
+
+            Exception exception = assertThrows(BooklistException.class, () -> {
+                defaultBooklist.addBookToAll(reader, newList);
+            });
+
+            String expectedMessage = "The book(s) Twilight, Jedi is already in list all";
+            String actualMessage = exception.getMessage();
+
+            assertNotNull(actualMessage);
+            assertEquals(expectedMessage, actualMessage);
+
+            Mockito.verify(data, Mockito.times(0)).addBookToCustomList(Mockito.any(Booklist.class), Mockito.any(User.class));
+            Mockito.verify(reader).getAllBooksList();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            assert(false);
+        }
+    }
+
+    @Test
+    public void testAddBookToReading() {
+        try {
+            Mockito.when(book1.getName()).thenReturn("Twilight");
+            Mockito.when(book2.getName()).thenReturn("Jedi");
+
+            newList.add(book1);
+            oldList.add(book2);
+
+            Mockito.when(reader.getInProgressList()).thenReturn(oldList);
+
+            defaultBooklist.addBookToInProgress(reader, newList);
+
+            Mockito.verify(data, Mockito.times(1)).addBookToReadingList(Mockito.any(Booklist.class), Mockito.any(User.class));
+            Mockito.verify(reader).getInProgressList();
+            assertEquals(2, oldList.size());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            assert(false);
+        }
+    }
+
+    @Test
+    public void testAddBooklistToReading() {
+        try {
+            Mockito.when(book1.getName()).thenReturn("Twilight");
+            Mockito.when(book2.getName()).thenReturn("Jedi");
+
+            newList.add(book1);
+            newList.add(book2);
+
+            Mockito.when(reader.getInProgressList()).thenReturn(oldList);
+
+            defaultBooklist.addBookToInProgress(reader, newList);
+
+            Mockito.verify(data, Mockito.times(1)).addBookToReadingList(Mockito.any(Booklist.class), Mockito.any(User.class));
+            Mockito.verify(reader).getInProgressList();
+            assertEquals(2, oldList.size());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            assert(false);
+        }
+    }
+
+    @Test
+    public void testAddDuplicateBookToReading() {
+        try {
+            Mockito.when(book1.getName()).thenReturn("Twilight");
+
+            oldList.setName("in progress");
+            newList.add(book1);
+            oldList.add(book1);
+
+            Mockito.when(reader.getInProgressList()).thenReturn(oldList);
+
+            Exception exception = assertThrows(BooklistException.class, () -> {
+                defaultBooklist.addBookToInProgress(reader, newList);
+            });
+
+            String expectedMessage = "The book(s) Twilight is already in list in progress";
+            String actualMessage = exception.getMessage();
+
+            assertNotNull(actualMessage);
+            assertEquals(expectedMessage, actualMessage);
+
+            Mockito.verify(data, Mockito.times(0)).addBookToReadingList(Mockito.any(Booklist.class), Mockito.any(User.class));
+            Mockito.verify(reader).getInProgressList();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            assert(false);
+        }
+    }
+
+    @Test
+    public void testAddMultipleDuplicateBookToReading() {
+        try {
+            Mockito.when(book1.getName()).thenReturn("Twilight");
+            Mockito.when(book2.getName()).thenReturn("Jedi");
+
+            oldList.setName("all");
+            newList.add(book1);
+            newList.add(book2);
+            oldList.add(book1);
+            oldList.add(book2);
+
+            Mockito.when(reader.getInProgressList()).thenReturn(oldList);
+
+            Exception exception = assertThrows(BooklistException.class, () -> {
+                defaultBooklist.addBookToInProgress(reader, newList);
+            });
+
+            String expectedMessage = "The book(s) Twilight, Jedi is already in list all";
+            String actualMessage = exception.getMessage();
+
+            assertNotNull(actualMessage);
+            assertEquals(expectedMessage, actualMessage);
+
+            Mockito.verify(data, Mockito.times(0)).addBookToReadingList(Mockito.any(Booklist.class), Mockito.any(User.class));
+            Mockito.verify(reader).getInProgressList();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            assert(false);
+        }
+    }
+
+    @Test
+    public void testAddBookToFinished() {
+        try {
+            Mockito.when(book1.getName()).thenReturn("Twilight");
+            Mockito.when(book2.getName()).thenReturn("Jedi");
+
+            newList.add(book1);
+            oldList.add(book2);
+
+            Mockito.when(reader.getFinishedList()).thenReturn(oldList);
+
+            defaultBooklist.addBookToFinished(reader, newList);
+
+            Mockito.verify(data, Mockito.times(1)).addBookToFinishedList(Mockito.any(Booklist.class), Mockito.any(User.class));
+            Mockito.verify(reader).getFinishedList();
+            assertEquals(2, oldList.size());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            assert(false);
+        }
+    }
+
+    @Test
+    public void testAddBooklistToFinished() {
+        try {
+            Mockito.when(book1.getName()).thenReturn("Twilight");
+            Mockito.when(book2.getName()).thenReturn("Jedi");
+
+            newList.add(book1);
+            newList.add(book2);
+
+            Mockito.when(reader.getFinishedList()).thenReturn(oldList);
+
+            defaultBooklist.addBookToFinished(reader, newList);
+
+            Mockito.verify(data, Mockito.times(1)).addBookToFinishedList(Mockito.any(Booklist.class), Mockito.any(User.class));
+            Mockito.verify(reader).getFinishedList();
+            assertEquals(2, oldList.size());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            assert(false);
+        }
+    }
+
+    @Test
+    public void testAddDuplicateBookToFinished() {
+        try {
+            Mockito.when(book1.getName()).thenReturn("Twilight");
+
+            oldList.setName("all");
+            newList.add(book1);
+            oldList.add(book1);
+
+            Mockito.when(reader.getFinishedList()).thenReturn(oldList);
+
+            Exception exception = assertThrows(BooklistException.class, () -> {
+                defaultBooklist.addBookToFinished(reader, newList);
+            });
+
+            String expectedMessage = "The book(s) Twilight is already in list all";
+            String actualMessage = exception.getMessage();
+
+            assertNotNull(actualMessage);
+            assertEquals(expectedMessage, actualMessage);
+
+            Mockito.verify(data, Mockito.times(0)).addBookToFinishedList(Mockito.any(Booklist.class), Mockito.any(User.class));
+            Mockito.verify(reader).getFinishedList();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            assert(false);
+        }
+    }
+
+    @Test
+    public void testAddMultipleDuplicateBookToFinished() {
+        try {
+            Mockito.when(book1.getName()).thenReturn("Twilight");
+            Mockito.when(book2.getName()).thenReturn("Jedi");
+
+            oldList.setName("all");
+            newList.add(book1);
+            newList.add(book2);
+            oldList.add(book1);
+            oldList.add(book2);
+
+            Mockito.when(reader.getFinishedList()).thenReturn(oldList);
+
+            Exception exception = assertThrows(BooklistException.class, () -> {
+                defaultBooklist.addBookToFinished(reader, newList);
+            });
+
+            String expectedMessage = "The book(s) Twilight, Jedi is already in list all";
+            String actualMessage = exception.getMessage();
+
+            assertNotNull(actualMessage);
+            assertEquals(expectedMessage, actualMessage);
+
+            Mockito.verify(data, Mockito.times(0)).addBookToFinishedList(Mockito.any(Booklist.class), Mockito.any(User.class));
+            Mockito.verify(reader).getFinishedList();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            assert(false);
+        }
     }
 }
