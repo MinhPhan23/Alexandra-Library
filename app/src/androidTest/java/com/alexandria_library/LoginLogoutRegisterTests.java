5a1893ee2d6eba92fd802ea7ad22d710ba7cf8ce
Andrei464 <aasholokhov2003@gmail.com>
2024-03-26 17:59:07 -0500

    made the system test actually check if the list is populated properly

--- app/src/androidTest/java/com/alexandria_library/LoginLogoutRegisterTests.java
+++ app/src/androidTest/java/com/alexandria_library/LoginLogoutRegisterTests.java
@@ -3,24 +3,26 @@ package com.alexandria_library;
 import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import static net.bytebuddy.matcher.ElementMatchers.is;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.hasEntry;
-import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertThrows;
 
 import androidx.test.espresso.AmbiguousViewMatcherException;
+import androidx.test.espresso.NoMatchingViewException;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.rule.ActivityTestRule;
 
-import com.alexandria_library.dso.Book;
 import com.alexandria_library.presentation.Authentication.LoginActivity;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.io.IOException;
 
 public class LoginLogoutRegisterTests {
 
@@ -69,7 +71,18 @@ public class LoginLogoutRegisterTests {
 
             //Show Empty List
             onView(withId(R.id.all_btn)).perform(click());
-            //withItemContent();
+            //should fail because the list is completely empty
+            Exception exception1 = assertThrows(NoMatchingViewException.class, ()->{
+                onView(withText("The Seven Husbands of Evalyn Hugo")).check(matches(isDisplayed()));
+            });
+
+            Exception exception2 = assertThrows(NoMatchingViewException.class, ()->{
+                onView(withText("To Kill a Mockingbird")).check(matches(isDisplayed()));
+            });
+
+            Exception exception3 = assertThrows(NoMatchingViewException.class, ()->{
+                onView(withText("The Fault in Our Stars")).check(matches(isDisplayed()));
+            });
 
             //Log Out
             onView(withId(R.id.account_btn)).perform(click());
@@ -84,6 +97,10 @@ public class LoginLogoutRegisterTests {
 
             //Show Non-Empty List
             onView(withId(R.id.all_btn)).perform(click());
+            //should find all 3 no problem
+            onView(withText("The Seven Husbands of Evalyn Hugo")).check(matches(isDisplayed()));
+            onView(withText("To Kill a Mockingbird")).check(matches(isDisplayed()));
+            onView(withText("The Fault in Our Stars")).check(matches(isDisplayed()));
 
             //Log Out
             onView(withId(R.id.account_btn)).perform(click());
@@ -98,6 +115,18 @@ public class LoginLogoutRegisterTests {
 
             //Show Empty List
             onView(withId(R.id.all_btn)).perform(click());
+            //should all fail again because the list is completely empty
+            Exception exception4 = assertThrows(NoMatchingViewException.class, ()->{
+                onView(withText("The Seven Husbands of Evalyn Hugo")).check(matches(isDisplayed()));
+            });
+
+            Exception exception5 = assertThrows(NoMatchingViewException.class, ()->{
+                onView(withText("To Kill a Mockingbird")).check(matches(isDisplayed()));
+            });
+
+            Exception exception6 = assertThrows(NoMatchingViewException.class, ()->{
+                onView(withText("The Fault in Our Stars")).check(matches(isDisplayed()));
+            });
         }
         catch(AmbiguousViewMatcherException e){
             e.printStackTrace();
