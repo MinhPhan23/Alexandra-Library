55e60d652c12538e12636e4d4361d1cf35781e51
Xiang <shix2@myumanitoba.ca>
2024-03-26 12:12:07 -0500

    fixed filter's bug

--- app/src/main/java/com/alexandria_library/presentation/MainActivity.java
+++ app/src/main/java/com/alexandria_library/presentation/MainActivity.java
@@ -371,8 +371,8 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onClick(View v) {
                 filterBookDisplay(tagsClicked, genresClicked);
-                tagsClicked.clear();
-                genresClicked.clear();
+//                tagsClicked.clear();
+//                genresClicked.clear();
 
             }
         });
